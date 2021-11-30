b7b31fd952f994aabd3ba432cdfac2842f9abcdc
Jagrut M Bhatt <64686051+JagrutBhatt@users.noreply.github.com>
2021-11-30 16:41:27 +0000

    1st version

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Selenium_Youtube_tranding
 Grab top 10 youtube trending video using Selenium. It's a part of Jovion.ML online course 
+
+
+Youtube link:
+https://www.youtube.com/watch?v=FcW-AXsirBE
--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("hello world")
+import requests
+from bs4 import BeautifulSoup
+# to create headless browser
+import selenium
+
+
+Youtube_Tranding_Url = "https://www.youtube.com/feed/trending"
+
+response = requests.get(Youtube_Tranding_Url)
+
+print('status code',response.status_code)
+
+with open('tranding.html','w') as f:
+  f.write(response.text)
+
+doc = BeautifulSoup(response.text,'html.parser')
+
+print('Page title',doc.title.text)
+
+#find all video divs
+video_divs = doc.find_all('div', class_ = "ytd-video-renderer")
+
+print(F'Found {len(video_divs)} Videos')
