99ff976c8aa5e5db49ffae6879a740307bcdfccb
Jagrut M Bhatt <64686051+JagrutBhatt@users.noreply.github.com>
2021-12-01 18:46:34 +0000

    v3

--- main_1.py
+++ main_1.py
@@ -1,17 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
 
 Youtube_Tranding_Url = "https://www.youtube.com/feed/trending"
+def get_driver():
+  chrome_options = Options()
+  chrome_options.add_argument('--no-sandbox')
+  chrome_options.add_argument('--headless')
+  chrome_options.add_argument('--disable-dev-shm-usage')
+  driver = webdriver.Chrome(options=chrome_options)
+  return driver
 
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-dev-shm-usage')
+def get_videos(driver):
+  VIDEO_DIV_TAG ='ytd-video-renderer'
+  print('Fatching the page')
+  driver.get(Youtube_Tranding_Url)
+  print('get video divs')
+  video_divs = driver.find_elements(By.TAG_NAME,VIDEO_DIV_TAG)
+  return video_divs
+  
+if __name__ == "__main__":
+  print("Creating driver")
+  driver = get_driver()
+  videos = get_videos(driver)
+  print(f'Found {len(videos)} Videos')
 
+  print("Parsing the 1st video")
+# title, url , thumbnail_url, channel, views, uploadded,discription
+  video = videos[0]
+  title_tag = video.find_element(By.ID,'video-title')
+  title = title_tag.text
+  video_url = title_tag.get_attribute('href')
+  print(title,video_url)
+  
 
+  
+  
 
-driver = webdriver.Chrome(options=chrome_options)
+  
 
-driver.get(Youtube_Tranding_Url)
-
-print('Page title:',driver.title)
